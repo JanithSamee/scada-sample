0ccf4436dbc60c29bd56e8b0aadcb7cc321f7780
janithsamee <janith.samee0@gmail.com>
2025-07-30 22:29:36 +0530

    working valve #01

--- src/components/scada/ToggleValveControl.ts
+++ src/components/scada/ToggleValveControl.ts
@@ -1,88 +1,39 @@
 import { dia, util } from "@joint/core";
 
-export default class ToggleValve extends dia.Element {
-  defaults() {
-    return {
-      ...super.defaults,
-      type: "ToggleValve",
-      size: { width: 100, height: 100 },
-      open: false,
-      attrs: {
-        body: {
-          cx: "calc(w/2)",
-          cy: "calc(h/2)",
-          rx: "calc(w/2)",
-          ry: "calc(h/2)",
-          stroke: "gray",
-          strokeWidth: 2,
-          fill: {
-            type: "radialGradient" as const,
-            stops: [
-              { offset: "70%", color: "white" },
-              { offset: "100%", color: "gray" }
-            ]
-          }
-        },
-       
-      }
-    };
-  }
-
-  preinitialize() {
-    this.markup = util.svg/* xml */ `
-      <foreignObject width="100" height="50" x="0" y="calc(h)">
-        <div class="jj-switch" xmlns="http://www.w3.org/1999/xhtml">
-          <button @selector="buttonOn" class="jj-switch-on">open</button>
-          <button @selector="buttonOff" class="jj-switch-off">close</button>
-        </div>
-      </foreignObject>
-    `;
-  }
-
-  initialize() {
-    super.initialize();
-
-    // Listen to changes in "open"
-    this.on("change:open", this.updateSwitch, this);
-
-    // Initialize state
-    // this.updateSwitch()
-
-    // Attach DOM events after rendering
-    this.on("change:attrs", this.bindEvents, this);
-  }
-
-  bindEvents(graph:any) {
-    const view = graph;
-    if (!view) return;
-    const { childNodes } = view;
-
-    if (childNodes.buttonOn && !childNodes.buttonOn._bound) {
-      childNodes.buttonOn.addEventListener("click", () => this.toggleOpen());
-      childNodes.buttonOn._bound = true;
-    }
-    if (childNodes.buttonOff && !childNodes.buttonOff._bound) {
-      childNodes.buttonOff.addEventListener("click", () => this.toggleOpen());
-      childNodes.buttonOff._bound = true;
-    }
-  }
-
-  toggleOpen() {
-    const isOpen = this.get("open");
-    this.set("open", !isOpen);
-  }
-
-  updateSwitch(graph:any) {
-    const isOpen = this.get("open");
-    const view = graph;
-    if (!view) return;
-    const { childNodes } = view;
-    if (childNodes.buttonOn && childNodes.buttonOff) {
-      childNodes.buttonOn.disabled = isOpen;
-      childNodes.buttonOff.disabled = !isOpen;
-    }
-    if (childNodes.switchLabel) {
-      childNodes.switchLabel.textContent = isOpen ? "Valve is Open" : "Valve is Closed";
-    }
-  }
-}
+		const ToggleValveControl = dia.HighlighterView.extend({
+			UPDATE_ATTRIBUTES: ["open"],
+			children: util.svg/* xml */ `
+        <foreignObject width="100" height="50">
+            <div class="jj-switch" xmlns="http://www.w3.org/1999/xhtml">
+                <div @selector="label" class="jj-switch-label" style=""></div>
+                <button @selector="buttonOn" class="jj-switch-on">open</button>
+                <button @selector="buttonOff" class="jj-switch-off">close</button>
+            </div>
+        </foreignObject>
+    `,
+			events: {
+				"click button": "onButtonClick",
+			},
+			highlight: function (cellView: any) {
+				this.renderChildren();
+				const { model } = cellView;
+				const { el, childNodes } = this;
+				const size = model.size();
+				const isOpen = model.get("open");
+				el.setAttribute(
+					"transform",
+					`translate(${size.width / 2 - 50}, ${size.height + 10})`
+				);
+				childNodes.buttonOn.disabled = !isOpen;
+				childNodes.buttonOff.disabled = isOpen;
+				childNodes.label.textContent = model.attr("label/text");
+			},
+			onButtonClick: function (event: any) {
+				event.stopPropagation();
+				const { model } = this.cellView;
+				const isOpen = model.get("open");
+				model.set("open", !isOpen);
+			},
+    });
+    
+    export default ToggleValveControl
